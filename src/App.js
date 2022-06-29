180e4a5217c2a50f3653137b3a13c26c829da036
Julian Elmasry <julian31186@gmail.com>
2022-06-29 10:38:27 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,8 +19,12 @@ function App() {
         >
           Learn React
         </a>
+        
+       
       </header>
+      
     </div>
+    
   );
 }
 
