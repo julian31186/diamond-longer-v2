9d4efddc393d3904ecd9ac63e4ab236b6ec24832
Julian Elmasry <julian31186@gmail.com>
2022-06-29 23:25:38 -0400

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Login from './components/Login';
+import Deso from 'deso-protocol';
+import { useState } from 'react';
+
 
 function App() {
+
+  const fetchLogin = async () => {
+    const deso = new Deso();
+    const request = 3;
+    const response = await deso.identity.login(request);
+    setPublicKey(response.key);
+    console.log(response)
+    return response;
+}
+
+ const [publicKey, setPublicKey] = useState();
+
   return (
     <div className="App">
-      <Header/>
-      <header className="App-header">
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-       
-      </header>
-      
+      <div> DiamondLonger</div>
+      <button onClick={fetchLogin}>Login</button>
+      <h1>BC1YLgze5LxcM1Gf4oCxA4cUvzoj6hnftXtv4rc3s7JBmbS9fM5F8fk</h1>
+      <button onClick={() => console.log(publicKey)}>{publicKey}Log Key</button>
     </div>
     
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,19 @@
+
+import Deso from 'deso-protocol';
+
+
 const Login = () => {
 
-
-
+    const fetchLogin = async () => {
+        const deso = new Deso();
+        const request = 3;
+        const response = await deso.identity.login(request);
+        return response;
+    }
 
     return (
-
+        <button onClick={fetchLogin}>Login</button>
     )
 }
+
+export default Login
