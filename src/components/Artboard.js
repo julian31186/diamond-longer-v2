15b9cdea083a94d467868f1843cf3b2e8d7cc3a4
Julian Elmasry <julian31186@gmail.com>
2022-07-01 02:10:11 -0400

    more styles to home page

--- src/components/Artboard.js
+++ src/components/Artboard.js
@@ -18,12 +18,19 @@ const Artboard = () => {
 
     return(
         <div class="flex mt-10 justify-center gap-6">
-            <div class="bg-white rounded-md w-652.5 h-592.48">
-
-
+            
+            <div class="flex flex-col justify-lef bg-white rounded-md w-652.5 h-592.48">
+                <div class="border-b-2 ">
+                <h1 class="flex mt-4 ml-4 font-bold mb-4">Sign in to start writing longer posts with DiamondLonger</h1>
+                </div>
+               <div class="flex flex-row mt-2 gap-2">
+               <button onClick={fetchLogin} class="ml-4 btn bg-twitblue border-0 btn-active text-white hover:bg-gray-400 gap-2 mt-5"><Icon height="20" width="24" icon="fa:diamond"/>Sign in with DeSo</button>
+               <p class="mt-7" >DiamondLonger is the easy way to share long messages</p>
+               </div>   
+                                                                                                                                                     
             </div>
             <div class="rounded-md bg-white w-64 h-32 ">
-                <p class="flex justify-center">Get Started</p>
+                <p class="flex justify-center mt-4 border-b-2">Get Started</p>
                 <button onClick={fetchLogin} class="btn bg-twitblue border-0 btn-active text-white hover:bg-gray-400 gap-2 mt-5"><Icon height="20" width="24" icon="fa:diamond"/>Sign in with DeSo</button>
             
 
